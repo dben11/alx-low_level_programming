4f4b814d4629e1fefca0421b4874c092e0e71a9c
dben <sam.dben@gmail.com>
2023-03-22 15:17:37 -0700

    0_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c) {
+  return putchar(c);
+}
+
+int main() {
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+
+  return 0;
+}
+
+
+
