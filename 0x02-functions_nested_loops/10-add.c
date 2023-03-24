ec4b829060ee5e7a6949762fa88fa5ee09335942
dben <sam.dben@gmail.com>
2023-03-24 13:48:22 -0700

    10.a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
