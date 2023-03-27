438b083f528d38fc67e33e5ca3383eb016a6329e
dben <sam.dben@gmail.com>
2023-03-27 15:15:03 -0700

    multiples of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiples of two integers
+ *@a: integer to be multiply b
+ *@b: intiger to be multiply a
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
