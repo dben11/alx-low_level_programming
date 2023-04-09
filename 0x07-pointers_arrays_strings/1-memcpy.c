7fa8d66776ea2703af32d51ee78ee7ec5b4dfe56
dben <sam.dben@gmail.com>
2023-04-09 15:43:39 -0700

    memset2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - functions that copies memry area
+ * @dest:memory destination
+ * @src:memory area
+ * @n:size of bytes memory
+ * Return: return dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+
+}
